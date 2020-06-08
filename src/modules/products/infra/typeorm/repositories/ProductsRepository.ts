e75a6197756673427e9b42b40ca27b30691e179e
Jhean Ramos <jheanr@gmail.com>
2020-06-08 19:19:55 +0100

    Resolução desafio

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -3,6 +3,7 @@ import { getRepository, Repository, In } from 'typeorm';
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
 import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import AppError from '@shared/errors/AppError';
 import Product from '../entities/Product';
 
 interface IFindProducts {
@@ -21,21 +22,67 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const findProduct = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return findProduct;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const orderProductsList = products.map(product => product.id);
+    const productsList = await this.ormRepository.find({
+      id: In(orderProductsList),
+    });
+
+    if (orderProductsList.length !== productsList.length) {
+      throw new AppError('One or more product is missing.');
+    }
+
+    return productsList;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productsList = await this.findAllById(products);
+
+    const productsToUpdate = productsList.map(productList => {
+      const productToUpdate = products.find(
+        product => product.id === productList.id,
+      );
+
+      if (!productToUpdate) {
+        throw new AppError('Product not found.');
+      }
+
+      if (productList.quantity < productToUpdate.quantity) {
+        throw new AppError('Product quantity higher than stock quantity.');
+      }
+
+      const newProduct = productList;
+      newProduct.quantity -= productToUpdate.quantity;
+
+      return newProduct;
+    });
+
+    await this.ormRepository.save(productsToUpdate);
+
+    return productsToUpdate;
   }
 }
 
